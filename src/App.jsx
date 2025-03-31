9156e740953ed65509edcb9204f4e2db146c224e
gustavo <gustavobrandassi@icloud.com>
2025-03-31 17:22:53 -0300

    primeiro commit do projeto ,base do html e inicio do CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import iconStar from "./assets/icon-star.svg"
+import { useState } from "react"
+import illustrationThankYou from "./assets/illustration-thank-you.svg"
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App(){
+  const [submited, setSubmited] = useState(false)
+  const [rateNote,setRateNote] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  function handleSubmit(){
+    if(rateNote !== 0){
+      setSubmited(true)
+      return
+    }
+    alert("please,choose a rate button! ")
+  }
+
+  function handleChangeRateNote(value){
+    setRateNote(value)
+  }
+
+  return(
+    submited === false ? (
+      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass ">
+        <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
+          <img src={iconStar} alt="icone da estrela" />
+        </div>
+
+        <h1 className="font-bold text-2xl mb-2.5">How did we do?</h1>
+        <p className="text-light-grey text-sn leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+        <div className="flex justify-between mb-6">
+          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(1)} />
+          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(2)} />
+          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(3)} />
+          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(4)} />
+          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(5)} />
+        </div>
+
+        <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange cursor-pointer transition duration-400" onClick={handleSubmit}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    ) : (
+      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center">
+        <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
+        <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {rateNote} out of 5!</p>
+        <h1 className="font-bold text-2xl mb-2.5">Thank You!</h1>
+        <p className="text-light-grey text-sn leading-1">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
   )
 }
-
-export default App
