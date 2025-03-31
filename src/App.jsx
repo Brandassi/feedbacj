554c0a129020c272800bf4bf68f4125d79bebfd1
gustavo <gustavobrandassi@icloud.com>
2025-03-31 17:31:18 -0300

    finalização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ export function App(){
         <p className="text-light-grey text-sn leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
         <div className="flex justify-between mb-6">
-          
           <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(1)} />
           <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(2)} />
           <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sn text-medium-grey font-bold focus:bg-medium-grey focus:text-white hover:bg-orange hover:text-white cursor-pointer sm:w-13 sm:h-13 transition duration-400" onClick={() => handleChangeRateNote(3)} />
@@ -40,7 +39,7 @@ export function App(){
         <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange cursor-pointer transition duration-400" onClick={handleSubmit}>Submit</button>
       </div>
     ) : (
-      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center">
+      <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center card-thank-you">
         <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
         <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {rateNote} out of 5!</p>
         <h1 className="font-bold text-2xl mb-2.5">Thank You!</h1>
